07fcf4666f0d16b42e5682e62f7825b5b70329f8
dakotaeckheart <deckheart@cpp.edu>
2020-07-05 19:07:32 -0700

    Example collision read for first car movement

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import sys
 import cv2
 import sim
 import time
+import datetime
 import numpy as np
 import matplotlib as mpl
 
@@ -26,14 +27,34 @@ errorCode, right_motor_handle = sim.simxGetObjectHandle(clientID, 'RightMotor', 
 
 if errorCode != sim.simx_return_ok:
     print('Error: ', errorCode)
+    
+errorCode, car_collision_handle = sim.simxGetCollisionHandle(clientID, 'Collision', sim.simx_opmode_oneshot_wait)
+
+if errorCode != sim.simx_return_ok:
+    print('Error: ', errorCode)
+
 
 time.sleep(2)
 sim.simxAddStatusbarMessage(clientID, 'Connected and running', sim.simx_opmode_oneshot)
-vel = 1
+vel = 10
 print("Forward")
 sim.simxSetJointTargetVelocity(clientID, left_motor_handle, -vel, sim.simx_opmode_streaming)
 sim.simxSetJointTargetVelocity(clientID, right_motor_handle, -vel, sim.simx_opmode_streaming)
-time.sleep(10)
+
+#12 second time frame to read if a collision occurred
+t = time.time()
+while time.time()-t < 12:
+    returnCode, collisionState = sim.simxReadCollision(clientID, car_collision_handle, sim.simx_opmode_streaming)
+    if(collisionState != 0):
+        print("Collision occurred")
+        time.sleep(1)
+        sys.exit()
+
+
+print(collisionState)
+if(collisionState != 0):
+    sys.exit("Collision occurred")
+    
 print("Left")
 sim.simxSetJointTargetVelocity(clientID, left_motor_handle, vel, sim.simx_opmode_streaming)
 time.sleep(10)
